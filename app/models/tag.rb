09627cf718ecd5ec43df0282a0e25ede0eb07384
yanina <laura.y.lp96@gmail.com>
2017-12-09 13:18:50 -0300

    cambios en vistas, maximo de caracteres en el modelo de tag y validaciones para crear una facultad

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,6 +1,6 @@
 class Tag < ApplicationRecord
 	has_and_belongs_to_many :questions
-	validates :nombre, :presence => {:message => "Usted debe ingresar un nombre!"}, length: {minimum: 1, maximum: 15, :message => "El nombre debe tener entre 1 y 15 caracteres!"}
+	validates :nombre, :presence => {:message => "Usted debe ingresar un nombre!"}, length: {minimum: 1, maximum: 30, :message => "El nombre debe tener entre 1 y 15 caracteres!"}
 	scope :noeliminados, -> {where(marca: false) }
 
 	def self.search(query)
--- app/models/university.rb
+++ app/models/university.rb
@@ -3,6 +3,10 @@ class University < ApplicationRecord
 has_many :users #una facultad puede pertenecer a varios users
 has_many :questions
 
+validates :nombre, :presence => {:message => "Usted debe ingresar un nombre!"}, length: {minimum: 1, maximum: 40, :message => "El nombre debe tener entre 1 y 40 caracteres!"}
+validates :ubicacion, :presence => {:message => "Usted debe ingresar una ubicacion!"}, length: {minimum: 1, maximum: 40, :message => "La ubicacion debe tener entre 1 y 40 caracteres!"}
+
+
 scope :noeliminados, -> {where(marca: false) }
 scope :max, -> {order("questions_count DESC")}
 
