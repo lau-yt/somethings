a9d72953010646a3fed5e8a464d45f075c16a104
yanina <laura.y.lp96@gmail.com>
2017-12-13 19:09:26 -0300

    add marca to heroku

--- db/migrate/20171208163839_addcolumanswer.rb
+++ db/migrate/20171208163839_addcolumanswer.rb
@@ -2,5 +2,6 @@ class Addcolumanswer < ActiveRecord::Migration[5.1]
   def change
   	add_column :questions, :answers_count,:integer,  :default => 0, :null => false
   	add_column :universities, :questions_count,:integer,  :default => 0, :null => false
+  	add_column :users, :marca, :boolean, :default => false, :null => false # migrar a heroku add
   end
 end
