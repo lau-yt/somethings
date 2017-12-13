d5e443096363955d296feb7da14f44a48ca8b79e
yanina <laura.y.lp96@gmail.com>
2017-12-13 18:14:25 -0300

    cambios menores

--- db/seeds.rb
+++ db/seeds.rb
@@ -47,3 +47,13 @@ Tag.create(nombre: 'psicologia')
 puts "Creo permisos"
 Permit.create(votes_pos: '15', comment: '20',  create_question_answer: '1', votes_neg: '100', adm_tag: '300', adm_uni: '500', modificar_contenido: '1000' )
 
+
+puts "Creo las ayudas"
+Help.create(titulo: 'Votos', descripcion: 'Los votos pueden ser positivos o negativos. Comenzaras con votos positivos y mientras mejor reputacion tengas por tu participacion en la pagina conseguiras poder votar negativamente!')
+Help.create(titulo: 'Comentarios', descripcion: 'Cuando llegues a cierta cantidad de puntos en tu cuenta, podras comentar tanto preguntas como respuestas')
+Help.create(titulo: 'Administrar etiquetas', descripcion: 'Podras crear etiquetas, eliminar y modificarlas. Esta opcion se encontrara en Ver perfil cuando cumplas los puntos necesarios!')
+Help.create(titulo: 'Administrar facultades', descripcion: 'Podras agregar, eliminar y modificar facultades al sitio de Educanlink! Esta opcion se encontrara en Ver perfil cuando cumplas los puntos necesarios!')
+Help.create(titulo: 'Modificar contenidos', descripcion: 'La opcion de editar y eliminar contenidos ya sean comentarios, preguntas y respuestas estara disponible al cumplir los puntos necesarios!')
+Help.create(titulo: 'Administrar denuncias', descripcion: 'Podras leer las denuncias de usuarios de menor puntaje y aplicar una medida correspondiente en caso de ser necesario!')
+Help.create(titulo: 'Eliminar cuenta del sitio', descripcion: 'Podras eliminar la cuenta de un usuario de menor puntaje en caso de que sea necesario')
+
