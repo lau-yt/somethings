23118038d2aa9d7c67f78d797207a219313fd1da
yanina <laura.y.lp96@gmail.com>
2017-12-14 12:26:01 -0300

    cambio ultima migracion para heroku

--- db/migrate/20171213124730_add_column_users.rb
+++ db/migrate/20171213124730_add_column_users.rb
@@ -1,5 +1,5 @@
 class AddColumnUsers < ActiveRecord::Migration[5.1]
   def change
-  	#add_column :users, :marca, :boolean,  :default => false, :null => false
+  	add_column :users, :marca, :boolean,  :default => false, :null => false
   end
 end
